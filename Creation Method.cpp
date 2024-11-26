00319ed0319a940d6b1aa7afc5cb6a0d45067267
ShinkorM <maksim20060627sinkora@gmail.com>
2024-11-26 10:41:00 +0300

    Add files via upload

---
+++ Creation Method.cpp
@@ -0,0 +1,29 @@
+﻿#include <vector>
+class Shape {
+public:
+    virtual void draw() = 0; // Абстрактный метод
+    virtual ~Shape() = default;
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        std::cout << "Drawing Circle\n";
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    void draw() override {
+        std::cout << "Drawing Rectangle\n";
+    }
+};
+
+class ShapeFactory {
+public:
+    static std::unique_ptr<Shape> createShape(const std::string& type) {
+        if (type == "Circle") return std::make_unique<Circle>();
+        if (type == "Rectangle") return std::make_unique<Rectangle>();
+        throw std::invalid_argument("Unknown shape type");
+    }
+};
